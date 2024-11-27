b0e47e4f927424f69df9c7e7cbd74e88637df3ab
Leonardo Talavera <leosistema281@gmail.com>
2024-11-26 22:47:19 -0600

    welcome add and agregate spanish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/leo-login/leo-login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' }, 
-  { path: 'login', component: LoginComponent }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent },
 ];
--- src/app/components/leo-login/leo-login.component.ts
+++ src/app/components/leo-login/leo-login.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertService } from '../../services/alert.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './leo-login.component.html',
@@ -9,20 +9,33 @@ import { AlertService } from '../../services/alert.service';
 export class LoginComponent {
   showPassword = false;
 
-  constructor(private alertService: AlertService) {}
+  constructor(private alertService: AlertService, private router: Router) {}
 
   togglePassword() {
     this.showPassword = !this.showPassword;
-    const passwordField: any = document.querySelector('.password');
-    passwordField.type = this.showPassword ? 'text' : 'password';
+    const passwordField: HTMLInputElement | null = document.querySelector('.password');
+    if (passwordField) {
+      passwordField.type = this.showPassword ? 'text' : 'password';
+    }
   }
 
-  onSubmit(event: Event) {
-    event.preventDefault(); 
-    this.hardcodeLogin();
+  onSubmit(event: Event, username: string, password: string) {
+    event.preventDefault();
+    this.hardcodeLogin(username, password);
   }
 
-  hardcodeLogin() {
-    this.alertService.showAlert('success', 'Welcome! You have successfully logged in.', 1500);
+  hardcodeLogin(username: string, password: string) {
+    if (username.trim() === '' || password.trim() === '') {
+      this.alertService.showAlert('error', 'Por favor, ingresa un usuario y contraseña.', 3000);
+      console.log("Campos vacíos");
+      return;
+    }
+
+    if (username === 'admin' && password === '1234') {
+      this.alertService.showAlert('success', 'Inicio de sesión exitoso. ¡Bienvenido!', 3000);
+      this.router.navigate(['/welcome']);
+    } else {
+      this.alertService.showAlert('error', 'Usuario o contraseña incorrectos.', 3000);
+    }
   }
 }
