42e142c222214e7c0a5b7a40a4fc0a082e817939
DaniDeveloper11 <angel.montes0498@gmail.com>
2024-11-29 12:23:44 -0600

    add: image to dlogin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './components/leo-login/leo-login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DaniLoginComponent } from './components/dani-login/dani-login/dani-login.component';
 
+
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'welcome', component: WelcomeComponent },
-  { path: 'dlogin',component: DaniLoginComponent}
+  { path: 'dlogin',component: DaniLoginComponent},
 ];
